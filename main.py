563b5e926ef7fa37518a27dd58551fb4a2f60bfb
Jose Luis Ferrete Olarte <joseluisferrete@gmail.com>
2021-09-07 17:15:48 +0000

    Second exercise

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-print("Day 1 - Python Print Function\nThe function is declared like this:\nprint('what to print')")
+print("Day 1 - String Manipulation")
+print("String Concatenation is done with the + sign.")
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
