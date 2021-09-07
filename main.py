4bbd72dd15700a18cedc843aa33014528fdecc56
Jose Luis Ferrete Olarte <joseluisferrete@gmail.com>
2021-09-07 17:27:49 +0000

    band-name-generator-end

--- main.py
+++ main.py
@@ -1,19 +1,4 @@
-# 🚨 Don't change the code below 👇
-a = input("a: ")
-b = input("b: ")
-# 🚨 Don't change the code above 👆
-
-####################################
-#Write your code below this line 👇
-
-c = a
-a = b
-b = c
-
-
-#Write your code above this line 👆
-####################################
-
-# 🚨 Don't change the code below 👇
-print("a: " + a)
-print("b: " + b)
+print("Welcome to the Band Name Generator.")
+street = input("What's name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+print("Your band name could be " + street + " " + pet)
