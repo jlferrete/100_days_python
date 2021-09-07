0383b6c77469467a9c3d387323aac7bdfcd34c6f
Jose Luis Ferrete Olarte <joseluisferrete@gmail.com>
2021-09-07 17:06:59 +0000

    Refactorized \n

--- main.py
+++ main.py
@@ -1,5 +1,1 @@
-print("Day 1 - Python Print Function")
-
-print("The function is declared like this:")
-
-print("print('what to print')")
+print("Day 1 - Python Print Function\nThe function is declared like this:\nprint('what to print')")
